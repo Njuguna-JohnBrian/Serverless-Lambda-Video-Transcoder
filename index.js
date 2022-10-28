4653f6f42e8f90d99083ff5f8d39f155a1921a0d
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-10-28 13:58:40 +0300

    Create elastic transcode pipeline

---
+++ index.js
@@ -0,0 +1,40 @@
+let AWS = require("aws-sdk");
+
+let elasticTranscoder = new AWS.ElasticTranscoder({ region: "us-east-1" });
+
+exports.handler = function (event, context, callback) {
+  console.log("Welcome");
+  let key = event.Records[0].s3.object.key;
+
+  //The input file may have spaces so hence need to replace them with '+'
+  let sourceKey = decodeURIComponent(key.replace(/\+/g, " "));
+
+  //remove the extension
+  let outputKey = sourceKey.split(".")[0];
+
+  let params = {
+    PipelineId: "1666946207334-ijbmnm",
+    Input: {
+      Key: sourceKey,
+    },
+    Outputs: [
+      {
+        Key: outputKey + "-1080p" + ".mp4",
+        PresetId: "1351620000001-000001", //Generic 1080p
+      },
+      {
+        Key: outputKey + "-720p" + ".mp4",
+        PresetId: "1351620000001-000010", //Generic 720p
+      },
+      {
+        Key: outputKey + "-web-720p" + ".mp4",
+        PresetId: "1351620000001-100070", //Web Friendly 720p
+      },
+    ],
+  };
+  elasticTranscoder.createJob(params, function (error, data) {
+    if (error) {
+      callback(error);
+    }
+  });
+};
